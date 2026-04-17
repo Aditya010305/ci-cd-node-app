fee275151c19d7dac740ca6159376b1f3fc4f6f1
Aditya Pratap Singh <adi0103singh@gmail.com>
2026-04-17 15:31:34 +0530

    Initial commit - Node app

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to CI/CD App 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "Server is running 🚀" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
